562c5c6baf702a7046700b646e9f958717bf5975
altese <readme42u@gmail.com>
2023-11-13 22:31:32 +0900

    💫 AbsorbPointer - 카드가 위로 쌓였을 경우 탭 하면 펼쳐지고, 펼쳐진 상태에서 탭하면 각 카드가 클릭됨.

--- flutter_animations_masterclass/lib/screens/wallet_screen.dart
+++ flutter_animations_masterclass/lib/screens/wallet_screen.dart
@@ -44,7 +44,7 @@ class _WalletScreenState extends State<WalletScreen> {
           onTap: _onExpand,
           child: Column(
             children: [
-              const CreditCard(bgColor: Colors.purple)
+              CreditCard(bgColor: Colors.purple, isExpanded: _isExpanded)
                   .animate(
                     delay: 1.5.seconds,
                     // target은 애니메이션 진행 정도. 0부터 1까지의 범위를 가진다.
@@ -52,14 +52,14 @@ class _WalletScreenState extends State<WalletScreen> {
                     target: _isExpanded ? 0 : 1,
                   )
                   .flipV(end: 0.1), // vertical flip
-              const CreditCard(bgColor: Colors.black)
+              CreditCard(bgColor: Colors.black, isExpanded: _isExpanded)
                   .animate(
                     delay: 1.5.seconds,
                     target: _isExpanded ? 0 : 1,
                   )
                   .flipV(end: 0.1)
                   .slideY(end: -0.8), // end: -1이면 첫번째 카드를 전부 덮음
-              const CreditCard(bgColor: Colors.blue)
+              CreditCard(bgColor: Colors.blue, isExpanded: _isExpanded)
                   .animate(
                     delay: 1.5.seconds,
                     target: _isExpanded ? 0 : 1,
@@ -79,74 +79,89 @@ class _WalletScreenState extends State<WalletScreen> {
 
 class CreditCard extends StatelessWidget {
   final Color bgColor;
+  final bool isExpanded;
 
-  const CreditCard({super.key, required this.bgColor});
+  const CreditCard(
+      {super.key, required this.bgColor, required this.isExpanded});
+
+  void _onTap() {
+    print('tap');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.only(bottom: 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: bgColor,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: Column(
-          children: [
-            const SizedBox(height: 100),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    // 카드들이 위로 쌓였을 경우에는 Container 위젯의 onTap 이벤트가 아니라
+    // Column의 onTap 이벤트를 실행시켜야 한다.
+    return AbsorbPointer(
+      // absorbing: true일 경우 이벤트를 캔슬시킴.
+      absorbing: !isExpanded,
+      child: GestureDetector(
+        onTap: _onTap,
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(bottom: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: bgColor,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+            child: Column(
               children: [
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                const SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Park Yejin',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      '**** **** *** **75',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-                Stack(
-                  // Clip.none: 원이 잘리는 것 방지!!
-                  clipBehavior: Clip.none,
-                  children: [
-                    Positioned(
-                      child: Container(
-                        width: 50,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.red,
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Park Yejin',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 20,
-                      child: Container(
-                        width: 50,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.amber,
+                        Text(
+                          '**** **** *** **75',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
+                      ],
                     ),
+                    Stack(
+                      // Clip.none: 원이 잘리는 것 방지!!
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned(
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 20,
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
