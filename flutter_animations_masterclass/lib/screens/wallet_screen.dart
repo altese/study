dec19e7cfa19adf24f5150bc1081cbbfed6d574f
altese <readme42u@gmail.com>
2023-11-13 22:11:41 +0900

    💫 target - 탭할 경우 펼쳐지고 드래그할 경우 위로 쌓임.

--- flutter_animations_masterclass/lib/screens/wallet_screen.dart
+++ flutter_animations_masterclass/lib/screens/wallet_screen.dart
@@ -9,6 +9,28 @@ class WalletScreen extends StatefulWidget {
 }
 
 class _WalletScreenState extends State<WalletScreen> {
+  bool _isExpanded = false;
+
+  // void _toggleExpanded() {
+  //   setState(() {
+  //     _isExpanded = !_isExpanded;
+  //   });
+  // }
+
+  // 카드들이 펼쳐진다.
+  void _onExpand() {
+    setState(() {
+      _isExpanded = true;
+    });
+  }
+
+  // 카드들이 위로 접힌다.
+  void _onShrink() {
+    setState(() {
+      _isExpanded = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +39,38 @@ class _WalletScreenState extends State<WalletScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const CreditCard(bgColor: Colors.purple).animate().flipV(end: 0.1),
-            const CreditCard(bgColor: Colors.black),
-            const CreditCard(bgColor: Colors.blue),
-          ]
-              .animate(interval: 500.ms)
-              .slideX(begin: -1, end: 0)
-              .fadeIn(begin: 0),
+        child: GestureDetector(
+          onVerticalDragEnd: (_) => _onShrink(),
+          onTap: _onExpand,
+          child: Column(
+            children: [
+              const CreditCard(bgColor: Colors.purple)
+                  .animate(
+                    delay: 1.5.seconds,
+                    // target은 애니메이션 진행 정도. 0부터 1까지의 범위를 가진다.
+                    // _isExpanded의 상태에 따라 target이 달라진다.
+                    target: _isExpanded ? 0 : 1,
+                  )
+                  .flipV(end: 0.1), // vertical flip
+              const CreditCard(bgColor: Colors.black)
+                  .animate(
+                    delay: 1.5.seconds,
+                    target: _isExpanded ? 0 : 1,
+                  )
+                  .flipV(end: 0.1)
+                  .slideY(end: -0.8), // end: -1이면 첫번째 카드를 전부 덮음
+              const CreditCard(bgColor: Colors.blue)
+                  .animate(
+                    delay: 1.5.seconds,
+                    target: _isExpanded ? 0 : 1,
+                  )
+                  .flipV(end: 0.1)
+                  .slideY(end: -0.8 * 2),
+            ]
+                .animate(interval: 500.ms)
+                .slideX(begin: -1, end: 0)
+                .fadeIn(begin: 0),
+          ),
         ),
       ),
     );
