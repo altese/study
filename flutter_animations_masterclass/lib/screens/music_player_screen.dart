3a08ef01302a49ac4d229195d7f29ed927b59fcf
altese <readme42u@gmail.com>
2023-11-27 21:26:49 +0900

    💫 AnimatedSwitcher - pageview 배경 애니메이션 추가

--- flutter_animations_masterclass/lib/screens/music_player_screen.dart
+++ flutter_animations_masterclass/lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -12,12 +14,42 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     viewportFraction: 0.8,
   );
 
+  // 현재 페이지를 저장합니다
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          // animatedSwitcher는 이전 자식과 새로운 자식 사이에 애니메이션을 적용한다.
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/covers/${_currentPage + 1}.jpg'),
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
           PageView.builder(
+            onPageChanged: _onPageChanged,
             controller: _pageController,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
@@ -29,6 +61,7 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     height: 300,
                     // width: 300,
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
                       image: DecorationImage(
                         fit: BoxFit.cover,
                         image: AssetImage('assets/covers/${index + 1}.jpg'),
@@ -36,9 +69,11 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     ),
                   ),
                   const SizedBox(height: 30),
-                  const Text('강아쥐', style: TextStyle(fontSize: 26)),
+                  const Text('강아쥐',
+                      style: TextStyle(fontSize: 26, color: Colors.white)),
                   const SizedBox(height: 10),
-                  const Text('강아쥐', style: TextStyle(fontSize: 18)),
+                  const Text('강아쥐',
+                      style: TextStyle(fontSize: 18, color: Colors.white)),
                 ],
               );
             },
